5586766b86dcdbfa61808f5e70c4a7b4352f4860
Marcin Kowalski <marcinkowalski189@gmail.com>
2023-01-19 20:59:39 +0100

    Clear code

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,7 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
 import rootReducer from './reducers';
-// import { countryApi } from '../components/List/listSlice';
-// import countriesSlice from '../components/List/listSlice';
 
 export const store = configureStore({
   reducer: rootReducer
